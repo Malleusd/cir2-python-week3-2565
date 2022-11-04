b086971f39d6f7dcde4878e07d7429dbeaa8887a
Malleusd <malleusdraconia63@gmail.com>
2022-11-04 13:34:44 +0700

    fon

---
+++ assignment.py
@@ -0,0 +1,22 @@
+#Example of Assignment Opertor
+a =  10
+
+#1.Assign Value
+b = a
+print('1.Assign Value b =',b) 
+
+#2.Add and assing value
+b += a
+print('2.Add and assing value b =',b) 
+
+#3.Subtract and Assgn value
+b -= a
+print('3.Subtract and Assgn value b =',b) 
+
+#4.Multply and Assign value
+b *= a
+print('4.Multply and Assign value b= ',b)
+
+#5.Division and Assign value
+b /= a
+print('5.Division and Assign value b= ',b)
